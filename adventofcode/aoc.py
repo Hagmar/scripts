0e6175d14d834412ae0aabf61e1ed8283bd97838
Magnus Hagmar <mhagmar@palantir.com>
2017-12-05 13:44:43 +0100

    Updated multiline template

--- adventofcode/aoc.py
+++ adventofcode/aoc.py
@@ -4,20 +4,23 @@ import requests as rq
 SESSION = 'Session cookie'
 URL_INPUT = 'http://adventofcode.com/2017/day/{}/input'
 
-PY_M = '''tot = 0
+PY_M = '''import sys
+tot = 0
 
-while True:
-    line = input().strip()
+ls = sys.stdin.read().rstrip().split('\n')
+
+for l in ls:
+    pass
 '''
 
 
 def main():
     args = parse_args()
 
-    if args.n:
-        res = rq.get(URL_INPUT.format(args.day), cookies={'session': SESSION})
-        with open('in{}'.format(args.day), 'w') as f:
-            f.write(res.text)
+    res = rq.get(URL_INPUT.format(args.day), cookies={'session': SESSION})
+    with open('in{}'.format(args.day), 'w') as f:
+        f.write(res.text)
+
     if args.m:
         name = args.name or '{}.py'.format(args.day)
         with open(name, 'w') as f:
@@ -27,7 +30,6 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', type=str, help='Script filename')
-    parser.add_argument('-n', action='store_true', help='New script')
     parser.add_argument('-m', action='store_true', help='Multiline')
     parser.add_argument('day', type=int, help='Day')
 
